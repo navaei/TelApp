440a14e2c2e215b215d4bff499d2455bddcc3aac
Meysam Navaei <meysam.navaei@hotmail.com>
2019-06-25 23:47:42 +0430

    fix other language resources

--- TMessagesProj/jni/tgnet/ConnectionsManager.cpp
+++ TMessagesProj/jni/tgnet/ConnectionsManager.cpp
@@ -1589,8 +1589,8 @@ bool ConnectionsManager::isIpv6Enabled() {
 
 void ConnectionsManager::initDatacenters() {
     Datacenter *datacenter;
-    //std::string mn_address("192.168.1.71");
-    std::string mn_address("144.76.9.215");
+    std::string mn_address("192.168.1.71");
+    //std::string mn_address("144.76.9.215");
     int mn_port = 8080;
     if (!testBackend) {
         if (datacenters.find(1) == datacenters.end()) {
